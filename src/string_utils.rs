03f66898b170673d120f1493372b0a9e2583f7c5
Werner Echezuria <werner_a_e@yahoo.es>
2018-02-12 17:45:33 -0300

    refactor apply_sha_256

--- src/string_utils.rs
+++ src/string_utils.rs
@@ -2,13 +2,8 @@ use parity_wordlist;
 use ring::{digest};
 
 pub fn apply_sha_256(input: &str) -> String {
-    let mut result: Vec<String> = Vec::new();
     let hash = digest::digest(&digest::SHA256, input.as_bytes());
-    for byte in hash.as_ref() {
-        let byte_to_vec = format!("{:02x}", byte);
-        result.push(byte_to_vec);
-    }
-    result.join("")
+    from_bytes_to_string(hash.as_ref())
 }
 
 pub fn generate_hash() -> String {
